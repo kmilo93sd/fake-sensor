0176baa0af2e676a3b5687009a9f8e8acf97c444
Camilo Alaniz <kmilo93sd@gmail.com>
2019-01-12 21:00:06 -0300

    fix named and responsabilities

--- Thermometer.php
+++ Thermometer.php
@@ -2,29 +2,8 @@
 
 class Thermometer
 {
-	private static $min = 10;
-	private static $max = 30;
-	private static $coldMin = 0;
-	private static $coldMax = 9.99;
-	private static $hotMin = 30.01;
-	private static $hotMax = 40;
-
-	public static function measure(Weather $weather)
+	public static function measure(Weather $weather):float
 	{
-		
-		if ($weather->isCold()) {
-			return static::randomInRange(static::$coldMin, static::$coldMax);
-		}
-
-		if ($weather->isHot()) {
-			return static::randomInRange(static::$hotMin, static::$hotMax);
-		}
-
-		return static::randomInRange(static::$min, static::$max);
-	}
-
-	private static function randomInRange($min, $max)
-	{
-		return mt_rand($min*10, $max*10)/10;
+		return $weather->themperature();
 	}
 }
--- Weather.php
+++ Weather.php
@@ -2,33 +2,66 @@
 
 class Weather
 {
-    protected $climateIndex;
+    protected $min = 10;
+
+	protected $max = 30;
+    
+    protected $coldMin = 0;
+    
+    protected $coldMax = 9.99;
+    
+    protected $hotMin = 30.01;
+    
+    protected $hotMax = 40;
+
+    protected $themperature;
 
     public function __construct(float $climateIndex)
     {
-        $this->climateIndex = $climateIndex;
+        $this->themperature = $this->generateThemperature($climateIndex);
     }
 
-    public static function random()
+    protected function generateThemperature(float $climateIndex):float
+    {
+        if($this->isTooCold($climateIndex)){
+            return $this->randomInRange($this->coldMin, $this->coldMax);
+        }
+
+        if($this->isTooHot($climateIndex)){
+            return $this->randomInRange($this->hotMin, $this->hotMax);
+        }
+
+        return $this->randomInRange($this->min, $this->max);
+    }
+
+    protected function isTooCold(float $climateIndex):bool
+    {
+        if($climateIndex <= 5){
+            return true;
+        }
+        return false;
+    }
+
+    protected function isTooHot(float $climateIndex):bool
+    {
+        if($climateIndex >=95){
+            return true;
+        }
+        return false;
+    }
+
+    protected function randomInRange($min, $max):float
+	{
+		return mt_rand($min*10, $max*10)/10;
+    }
+    
+    public static function random():Weather
     {
         return new Weather(mt_rand(0, 100));
     }
 
-    public function isCold():bool
+    public function themperature():float
     {
-        if ($this->climateIndex <= 5) {
-			return true;
-        }
-        
-        return false;
-    }
-
-    public function isHot():bool
-    {
-        if($this->climateIndex >= 95){
-            return true;
-        }
-
-        return false;
+        return $this->themperature;
     }
 }
